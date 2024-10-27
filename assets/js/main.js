c4592bf95351830a91dfc45ad1b12c18caf59425
faisal <faisaldevbhaluka@gmail.com>
2024-10-27 23:49:30 +0600

    now almost complite desgin bmi clinic website in html and css

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+(function ($) {
+"use strict";
+
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+ //Navber//
+(() =>{
+ 
+	const openNavMenu = document.querySelector(".open-nav-menu"),
+	closeNavMenu = document.querySelector(".close-nav-menu"),
+	navMenu = document.querySelector(".main-menu"),
+	menuOverlay = document.querySelector(".menu-overlay"),
+	mediaSize = 991;
+  
+	openNavMenu.addEventListener("click", toggleNav);
+	closeNavMenu.addEventListener("click", toggleNav);
+	// close the navMenu by clicking outside
+	menuOverlay.addEventListener("click", toggleNav);
+  
+	function toggleNav() {
+	  navMenu.classList.toggle("open");
+	  menuOverlay.classList.toggle("active");
+	  document.body.classList.toggle("hidden-scrolling");
+	}
+  
+	navMenu.addEventListener("click", (event) =>{
+		if(event.target.hasAttribute("data-toggle") && 
+		  window.innerWidth <= mediaSize){
+		  // prevent default anchor click behavior
+		  event.preventDefault();
+		  const menuItemHasChildren = event.target.parentElement;
+		  // if menuItemHasChildren is already expanded, collapse it
+		  if(menuItemHasChildren.classList.contains("active")){
+			collapseSubMenu();
+		  }
+		  else{
+			// collapse existing expanded menuItemHasChildren
+			if(navMenu.querySelector(".menu-item-has-children.active")){
+			collapseSubMenu();
+			}
+			// expand new menuItemHasChildren
+			menuItemHasChildren.classList.add("active");
+			const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+			subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+		  }
+		}
+	});
+	function collapseSubMenu(){
+	  navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+	  .removeAttribute("style");
+	  navMenu.querySelector(".menu-item-has-children.active")
+	  .classList.remove("active");
+	}
+	function resizeFix(){
+	   // if navMenu is open ,close it
+	   if(navMenu.classList.contains("open")){
+		 toggleNav();
+	   }
+	   // if menuItemHasChildren is expanded , collapse it
+	   if(navMenu.querySelector(".menu-item-has-children.active")){
+			collapseSubMenu();
+	   }
+	}
+  
+	window.addEventListener("resize", function(){
+	   if(this.innerWidth > mediaSize){
+		 resizeFix();
+	   }
+	});
+  
+  })();
+
+// Slides //
+  $('.slides-wrapper').slick({
+    arrows: true,
+    dots: false,
+    autoplay: false,
+    speed: 300,
+    slidesToShow: 4,
+    prevArrow: '<span class="priv_arrow"><i class="fa-light fa-circle-arrow-right"></i></span>',
+      nextArrow: ' <span class="next_arrow"><i class="fa-light fa-circle-arrow-left"></i></span>',
+    responsive: [
+      {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+      },
+      {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false,
+      }
+      },
+      {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+      }
+      }
+    ]
+    });
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
